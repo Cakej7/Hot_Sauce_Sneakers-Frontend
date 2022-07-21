2a5c56ef3e6ac1264dd4a29e66bd43abf5fc4505
Jacob Woehrle <woehrle17@gmail.com>
2022-07-20 19:39:22 -0700

    added single product view

--- src/components/SingleProduct.js
+++ src/components/SingleProduct.js
@@ -6,10 +6,7 @@ const products = [
         name: `AIR JORDAN 1 RETRO HIGH OG 'STAGE HAZE'`,
         brand: `Air Jordan`,
         image: `https://cdn.flightclub.com/3000/TEMPLATE/299069/1.jpg`,
-        price: 190.00,
-        stock: 5,
-        size: 11,
-        addedToCart: false
+        price: 190.00
     },
 ]
 
@@ -23,40 +20,37 @@ const SingleProduct = () => {
             {
                 products.map(({ name, brand, image, price }) => {
                     return (
-                    <div key={name}>
-                        <img src={image} alt="" width="500" height="400"></img>
-                        <h1>{brand}</h1>
-                        <h3>{name}</h3>
-                        <h2>${price}</h2>
+                        <div key={name}>
+                            <img src={image} alt="" width="500" height="400"></img>
+                            <h1>{brand}</h1>
+                            <h3>{name}</h3>
+                            <h2>${price}</h2>
 
-                        <form>
-                            <label>Size: </label>
-                            <input
-                                type='number'
-                                value={size}
-                                onChange={(e) => setSize(e.target.value)}
-                            />
+                            <form>
+                                <label>Size: </label>
+                                <input
+                                    type='number'
+                                    value={size}
+                                    onChange={(e) => setSize(e.target.value)}
+                                />
 
-                            <label>Stock: </label>
-                            <input
-                                type='number'
-                                value={stock}
-                                onChange={(e) => setStock(e.target.value)}
-                            />
-                            <button
-                                onClick={(e) => {
-                                e.preventDefault();
-                                // addProductToCart()
-                                navigate('../cart')
-                                }}
-                                >Add to cart
-                            </button>
-                        </form>
-                        
-
-                    </div>
-            )
-                    
+                                <label>Stock: </label>
+                                <input
+                                    type='number'
+                                    value={stock}
+                                    onChange={(e) => setStock(e.target.value)}
+                                />
+                                <button
+                                    onClick={(e) => {
+                                    e.preventDefault();
+                                    // addProductToCart()
+                                    navigate('../cart')
+                                    }}
+                                    >Add to cart
+                                </button>
+                            </form>
+                        </div>
+                    )
                 })
             }
         </>
